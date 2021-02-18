c1d977f5b4ce50f648ec586684399c8f625d7ed1
Barskih-ink <72916461+Barskih-ink@users.noreply.github.com>
2021-02-18 12:13:37 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+#*Тут либы качаем*#
 import cv2 as cv
-import numpy as np
+import numpy as np 
 
 screenshot = cv.imread('Senko.png', cv.IMREAD_UNCHANGED)
 searchface = cv.imread('Senkoface.png', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(screenshot, searchface, cv.TM_CCOEFF_NORMED)
 
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result) 
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)\
 
-print('Координата: %s' % str(max_loc))
+
+#*Вычисление координат Senkoface*#
+print('Координата: %s' % str(max_loc))   
 print('Ебаната: %s' % max_val)
 
-ebanata = 0.3
+
+ebanata = 0.36                          #вписыываем сюда положение
 if max_val >= ebanata:
     print('Вот она! Туточки!')
     face_w = searchface.shape[1]
@@ -23,10 +27,10 @@ if max_val >= ebanata:
 
  
     cv.rectangle(screenshot, top_left, bottom_right, 
-                    color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
+                    color=(0, 100, 0), thickness=4, lineType=cv.LINE_4)
 
   
-    cv.imwrite('result.jpg', screenshot)
+    cv.imwrite('result.jpg', screenshot) #результат поиска
 
 else:
-    print('Тут нету Сенко!')
+    print('Нассы себе на лицо!')
