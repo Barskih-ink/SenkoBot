5b917e97c59e283b18588f4b296572a2447d4c51
Barskih-ink <72916461+Barskih-ink@users.noreply.github.com>
2021-02-18 20:31:04 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-#*Тут либы качаем*#
 import cv2 as cv
-import numpy as np 
-
-screenshot = cv.imread('Senko.png', cv.IMREAD_UNCHANGED)
-searchface = cv.imread('Senkoface.png', cv.IMREAD_UNCHANGED)
-
-result = cv.matchTemplate(screenshot, searchface, cv.TM_CCOEFF_NORMED)
-
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)\
+import numpy as np
 
 
-#*Вычисление координат Senkoface*#
-print('Координата: %s' % str(max_loc))   
-print('Ебаната: %s' % max_val)
+
+haystack_img = cv.imread('moreSenko.png', cv.IMREAD_UNCHANGED)
+needle_img = cv.imread('Senkoface', cv.IMREAD_UNCHANGED)
 
 
-ebanata = 0.36                          #вписыываем сюда положение
-if max_val >= ebanata:
-    print('Вот она! Туточки!')
-    face_w = searchface.shape[1]
-    face_h = searchface.shape[0]
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
 
-    
-    top_left = max_loc
-    bottom_right = (top_left[0] + face_w, top_left[1] + face_h)
 
- 
-    cv.rectangle(screenshot, top_left, bottom_right, 
-                    color=(0, 100, 0), thickness=4, lineType=cv.LINE_4)
+threshold = 0.17
+
+locations = np.where(result <= threshold)
+
+locations = list(zip(*locations[::-1]))
+print(locations)
+
+if locations:
+    print('Found needle.')
+
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
 
   
-    cv.imwrite('result.jpg', screenshot) #результат поиска
+    for loc in locations:
+       
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+   
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+
+    cv.imshow('Matches', haystack_img)
+    cv.waitKey()
+    cv.imwrite('result.jpg', haystack_img)
 
 else:
-    print('Нассы себе на лицо!')
+    print('Needle not found.')
