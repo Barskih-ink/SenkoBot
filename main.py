5eadc7d7bfecaffd3b45b8298271963c08e1a44c
Barskih-ink <72916461+Barskih-ink@users.noreply.github.com>
2021-02-18 20:31:47 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
-import cv2 as cv
 import numpy as np
+import cv2
+from mss.linux import MSS as mss
+from PIL import Image
+import time
+import pyautogui as pg
+import cv2
+import mss
+import numpy
 
+template = cv2.imread("Senkoface.png", cv2.IMREAD_GRAYSCALE)
+w, h = template.shape[::-1]
 
+with mss.mss() as sct:
+    monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
 
-haystack_img = cv.imread('moreSenko.png', cv.IMREAD_UNCHANGED)
-needle_img = cv.imread('Senkoface', cv.IMREAD_UNCHANGED)
-
-
-result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
-
-
-threshold = 0.17
-
-locations = np.where(result <= threshold)
-
-locations = list(zip(*locations[::-1]))
-print(locations)
-
-if locations:
-    print('Found needle.')
-
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
-    line_color = (0, 255, 0)
-    line_type = cv.LINE_4
-
-  
-    for loc in locations:
-       
-        top_left = loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-   
-        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
-
-    cv.imshow('Matches', haystack_img)
-    cv.waitKey()
-    cv.imwrite('result.jpg', haystack_img)
-
-else:
-    print('Needle not found.')
+    while "Screen capturing":
+        last_time = time.time()
+        img = numpy.array(sct.grab(monitor))
+        cv2.imshow("OpenCV/Numpy normal", img)
+        print("fps: {}".format(1 / (time.time() - last_time)))
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= 0.7)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+        cv2.imshow("Frame", img)
+        key = cv2.waitKey(1)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
